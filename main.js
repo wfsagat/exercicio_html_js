ed250b37c7bac39c034f98eb6edceff489b52e5d
wilder filho <wilderfilhosagat@gmail,com>
2023-05-24 09:06:54 -0300

    adição do nome da branch

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function validarFormulario() {
     var numeroA = parseInt(document.getElementById("numeroA").value);
     var numeroB = parseInt(document.getElementById("numeroB").value);
     if (numeroB > numeroA) {
+    alert("Sua resposta esta correta")
     return true;
     } else {
     alert("O número B deve ser maior que o número A!");
